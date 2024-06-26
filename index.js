8a922d715952bb5087f13d054fdf566d307ea219
manishraj <wingsmanish11@gmail.com>
2024-06-26 22:37:38 +0530

    school_erp

---
+++ index.js
@@ -0,0 +1,13 @@
+// function toggleReadMore(event) {
+//     const button = event.currentTarget;
+//     const container = button.previousElementSibling;
+
+//     if (container.classList.contains('expanded')) {
+//         container.classList.remove('expanded');
+//         button.innerText = 'Read more';
+//     } else {
+//         container.classList.add('expanded');
+//         button.innerText = 'Read less';
+//     }
+// }
+
